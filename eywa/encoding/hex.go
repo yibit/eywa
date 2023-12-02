7943b176ceab43cea03661e52671a3cfda4a6130
yibit <yibitx@126.com>
2023-12-02 23:48:32 +0800

    Fixed HexDecode

--- eywa/encoding/hex.go
+++ eywa/encoding/hex.go
@@ -4,10 +4,15 @@ import (
 	"encoding/hex"
 )
 
-func HexEncode(src string) string {
-	return hex.EncodeToString([]byte(src))
+func HexEncode(s string) string {
+	return hex.EncodeToString([]byte(s))
 }
 
-func HexDecode(src string) string {
-	return hex.EncodeToString([]byte(src))
+func HexDecode(s string) string {
+	data, err := hex.DecodeString(s)
+	if err != nil {
+		return ""
+	}
+
+	return string(data)
 }
